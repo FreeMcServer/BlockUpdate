034df8777a652cf748cfc38d5beb0913c269766e
Nuno Facha <nunofacha@gmail.com>
2021-12-14 13:12:09 -0100

    Working S3 upload

--- src/s3/S3Uploader.ts
+++ src/s3/S3Uploader.ts
@@ -10,6 +10,7 @@ class S3Uploader {
         }
         console.log("Uploading to S3");
         let rx = await execSync('s3cmd sync --host=' + process.env.S3_ENDPOINT + ' --host-bucket=' + process.env.S3_ENDPOINT + ' --access_key=' + process.env.S3_KEY + ' --secret_key=' + process.env.S3_SECRET + ' -v ' + localFolder + ' s3://' + process.env.S3_BUCKET + '/' + dstFolder + '/');
+        console.log(rx.toString());
     }
 }
 
