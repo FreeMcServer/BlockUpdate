04852eed6721a2d52f6db095fcfcfc735e28d69a
Nuno Facha <nunofacha@gmail.com>
2021-12-17 15:54:44 -0100

    Re-enable disabled variants

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 import * as fs from "fs";
 import Purpur from "./purpur/Purpur";
+import Spigot from "./spigot/Spigot";
+import Paper from "./paper/Paper";
 
 async function start() {
     if (!fs.existsSync("./out")) {
         fs.mkdirSync("./out");
     }
 
-    // let spigot = new Spigot();
-    // await spigot.init();
-    //
-    // let paper = new Paper();
-    // await paper.init();
+    let spigot = new Spigot();
+    await spigot.init();
+
+    let paper = new Paper();
+    await paper.init();
 
     let purpur = new Purpur();
     await purpur.init();
