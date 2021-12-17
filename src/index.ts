1a3f6b7bf59080bf704cfb6ac78a7e553fc717d5
Nuno Facha <nunofacha@gmail.com>
2021-12-17 15:55:40 -0100

    Cleanup

--- src/index.ts
+++ src/index.ts
@@ -20,16 +20,5 @@ async function start() {
     console.log("Done!");
 }
 
-// console.log(execSync("update-alternatives --display java").toString());
-// console.log(execSync("ls /usr/lib/jvm/").toString());
-start();
 
-// let versions = ['1.17.1'];
-// for (let i = 0; i < versions.length; i++) {
-//     let version = versions[i];
-//     exec('java -jar ./buildtools/BuildTools.jar --rev '+version, (err, stdout, stderr) => {
-//         // your callback
-//         console.log(stdout);
-//         console.log('READY for '+version);
-//     });
-// }
+start();
