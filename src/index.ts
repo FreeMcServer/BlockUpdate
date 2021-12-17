287a3c639a997518c4a464b2fea18f87bf1a5808
Nuno Facha <nunofacha@gmail.com>
2021-12-17 15:54:21 -0100

    Only upload if there are changes

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,16 @@
 import * as fs from "fs";
 import Purpur from "./purpur/Purpur";
-import Spigot from "./spigot/Spigot";
-import Paper from "./paper/Paper";
 
 async function start() {
     if (!fs.existsSync("./out")) {
         fs.mkdirSync("./out");
     }
 
-    let spigot = new Spigot();
-    await spigot.init();
-
-    let paper = new Paper();
-    await paper.init();
+    // let spigot = new Spigot();
+    // await spigot.init();
+    //
+    // let paper = new Paper();
+    // await paper.init();
 
     let purpur = new Purpur();
     await purpur.init();
